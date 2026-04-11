6d09fe8d3bb3ae119af50dee14ed26a40a3d71d6
sahitero <eronita.sahiti04@gmail.com>
2026-04-12 00:26:01 +0200

    die Mails untereinander

--- views/home.py
+++ views/home.py
@@ -4,10 +4,10 @@ st.title("Der Labordedektiv - Home")
 
 
 
-"""
+st.markdown("""
 Diese App wurde von folgenden Personen entwickelt:
-Eronita Sahiti (sahitero@stundets.zhaw.ch)
-Lilia Totila  (totillil@students.zhaw.ch)
-Lia Bütikofer (buetilia@students.zhaw.ch)
-Vanessa Cakoncev (cakonvan@stundents.zhaw.ch)
-"""
+* **Eronita Sahiti** (sahitero@students.zhaw.ch)
+* **Lilia Totila** (totillil@students.zhaw.ch)
+* **Lia Bütikofer** (buetilia@students.zhaw.ch)
+* **Vanessa Cakoncev** (cakonvan@students.zhaw.ch)
+""")
