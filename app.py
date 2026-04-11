0e6c6322d2c890f52fdad5ae22d68d70ac21579f
sahitero <eronita.sahiti04@gmail.com>
2026-04-12 00:23:40 +0200

    App Umbenannt

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ if 'data_df' not in st.session_state:
 
 
 pg_home = st.Page("views/home.py", title="Home", icon=":material/home:", default=True)
-pg_second = st.Page("views/Neue-App.py", title="Neue App", icon=":material/info:")
+pg_second = st.Page("views/Der Labordedektiv.py", title="Der Labordedektiv", icon=":material/info:")
 
 pg = st.navigation([pg_home, pg_second])
 pg.run()
